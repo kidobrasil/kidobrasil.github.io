3fbce6a5d81e5a5dc6dedda8b03eab6ee3f354aa
vinex <notrealmailfornow@proton.me>
2024-10-22 12:00:30 -0300

    oppacity

--- js/home.js
+++ js/home.js
@@ -38,8 +38,8 @@ const particlesMaterial = new THREE.PointsMaterial({
     map: particleTexture,
     vertexColors: true,
     transparent: true,
-    alphaTest: 0.6,
-    opacity: 0.6,
+    alphaTest: 0.8,
+    opacity: 0.8,
 });
 
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
